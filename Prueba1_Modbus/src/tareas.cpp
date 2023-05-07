f8baa3ce676546efb7f34e68c71611d1300703f7
Hayepez17 <hayepez17@gmail.com>
2023-05-07 18:16:56 -0400

    Add Comments

--- Prueba1_Modbus/src/tareas.cpp
+++ Prueba1_Modbus/src/tareas.cpp
@@ -18,7 +18,7 @@ void TareaEntradaDatos(void *Parametro)
 
     while (1)
     {
-        //Iteración valores ADCs
+        // Iteración valores ADCs
         for (int j = 0; j < NumeroMuestras; j++)
         {
 
@@ -26,12 +26,12 @@ void TareaEntradaDatos(void *Parametro)
             promedio2 += adc_value2 = adc1_get_raw(CH2);
             promedio3 += adc_value3 = adc1_get_raw(CH3);
         }
-//Calculo Promedio de 100 muestras
+        // Calculo Promedio de 100 muestras
         promedio1 /= NumeroMuestras;
         promedio2 /= NumeroMuestras;
         promedio3 /= NumeroMuestras;
 
-//Establezco límite superior de entrada
+        // Establezco límite superior de entrada
         if (promedio1 > 4095)
             promedio1 = 4095;
         if (promedio2 > 4095)
@@ -39,13 +39,13 @@ void TareaEntradaDatos(void *Parametro)
         if (promedio3 > 4095)
             promedio3 = 4095;
 
-//Escribo en el Imput Register
+        // Escribo en el Imput Register
         MBInputRegister[0].Val = promedio1;
         MBInputRegister[1].Val = promedio2;
         MBInputRegister[2].Val = promedio3;
 
-        //printf("valor adc3 crudo: %d\n",adc_value3);
-        //printf("valor adc3 suave: %d\n",promedio3);
+        // printf("valor adc3 crudo: %d\n",adc_value3);
+        // printf("valor adc3 suave: %d\n",promedio3);
 
         vTaskDelay(1000 / portTICK_PERIOD_MS);
     }
